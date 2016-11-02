4f44fcd37175ac8ec05e33baf46e02403587ff47
nathan mcninch <nathanmcninch@protonmail.com>
2016-11-01 23:04:40 -0700

    mouse over rect

--- week8/empty/sketch.js
+++ week8/empty/sketch.js
@@ -1,7 +1,19 @@
-function setup() {
+//detect when the mouse enters a rectangle
+var x = 100
+var y = 50
+var rsize = 100
 
+function setup() {
+  createCanvas(500, 240)
+  background(200)
+  noStroke()
 }
 
 function draw() {
-  
+  rect(x, y, rsize, rsize)
+  if (mouseX > x && mouseX < x + rsize && mouseY > y && mouseY < y + rsize) {
+    fill(20)
+  } else {
+    fill(100)
+  }
 }
